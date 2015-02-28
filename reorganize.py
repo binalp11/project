00fed8a53d1aa93ee26ea65c90a092838a62f66f
Binal Patel <binalp11@gmail.com>
2015-02-28 05:20:47 +0000

    This script reorganizes the data using regex so each column is seperated by tabs

---
+++ reorganize.py
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+#import packages    
+import re
+
+
+#open new file to send output to
+clean_data  = open('organized_data.txt', 'w')
+
+#define a funciton to use regex and fix data into columns seperated by tabs
+def fix_my_data():
+    clean_data  = open('organized_data.txt', 'w')
+    #open data set
+    data_set = open("data_w_o_header.txt").read()
+    #use regex to fix spacing between columns
+    fixed = re.sub(r'"(\d+)","(.*?)","(.*?)","\s+([\d\.\,]+)","\s+([\d\.,]+)","\s+([\d\.\,]+)"', r'\1\t\2\t\3\t\4\t\5\t\6', str(data_set))
+    #send output to new file
+    clean_data.write(fixed + '\n')  
+    clean_data.close()
+
+
+#run function
+fix_my_data()
+
+def fix_1st_line():
+    data_set = open("organized_data.txt")
+    unfix = data_set.read()
+    fix_1st = re.sub(r'"(\w+)","(\w+)","(\w+)","(\w+\s\w+)","(\w+)","(\$)"',r'\1\t\2\t\3\t\4\t\5\t\6', str(unfix))
+    clean_data  = open('organized_data.txt', 'w')
+    clean_data.write(fix_1st + '\n')
+
